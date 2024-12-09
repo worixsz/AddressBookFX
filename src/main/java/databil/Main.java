f434ac47b326c8fe5ea0cfec7cfb61f41ab18282
azidinkun <azidinkun@icloud.com>
2024-12-10 04:12:47 +0600

    refactor the details into constructor

--- src/main/java/databil/Main.java
+++ src/main/java/databil/Main.java
@@ -4,14 +4,22 @@ import databil.ui.MainControlPane;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import repository.CheckActionMove;
+import repository.CreateContactMove;
 
 public class Main extends Application {
 
+    private CreateContactMove contactService;
+    private CheckActionMove checkActionMove;
 
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(new MainControlPane(), 640, 480);
+
+        this.contactService = new CreateContactMove();
+        this.checkActionMove = new CheckActionMove();
+
+
+        Scene scene = new Scene(new MainControlPane(contactService, checkActionMove), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
