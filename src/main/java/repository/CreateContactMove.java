7e0b7abba5cf0c917f05fc14b17be9cadab0d91d
azidinkun <azidinkun@icloud.com>
2024-12-13 18:16:30 +0600

    made the code shorter

--- src/main/java/repository/CreateContactMove.java
+++ src/main/java/repository/CreateContactMove.java
@@ -10,18 +10,17 @@ import java.util.List;
 public class CreateContactMove implements CreateAction {
 
     private final FileService fileService;
+    private final List<Contact> contactsList;
 
     public CreateContactMove() {
         this.fileService = new FileService();
+        this.contactsList = fileService.read() != null ? fileService.read() : new ArrayList<>();
     }
 
     @Override
     public void createContact(List<Contact> contacts) {
-        List<Contact> existingContacts = fileService.read();
-        if (existingContacts == null) {
-            existingContacts = new ArrayList<>();
-        }
-        existingContacts.addAll(contacts);
-        fileService.write(existingContacts);
+
+        contactsList.addAll(contacts);
+        fileService.write(contactsList);
     }
 }
