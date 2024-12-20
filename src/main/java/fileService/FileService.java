c9d77493530234f52d3c7c4d134b68a84ede68b9
azidinkun <azidinkun@icloud.com>
2024-12-20 13:21:39 +0600

    Implemented reactive READ method with details

--- src/main/java/fileService/FileService.java
+++ src/main/java/fileService/FileService.java
@@ -3,7 +3,6 @@ package fileService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.sun.source.util.TaskListener;
 import io.reactivex.rxjava3.core.BackpressureStrategy;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.FlowableEmitter;
@@ -11,7 +10,6 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 import model.Contact;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,29 +22,55 @@ public class FileService {
 
     }
 
+    /**
+     * Реактивный метод для чтения списка контактов из файла.
+     * Используется Flowable для выполнения операции в фоновом потоке (Schedulers.io).
+     * @return List<Contact> - список контактов.
+     */
+
     public List<Contact> read() {
-
         try {
-            File file = new File("contacts.json");
-            if (!file.exists()) {
-                return new ArrayList<>();
-            }
 
-            JsonNode rootNode = objectMapper.readTree(file);
-            JsonNode contactsNode = rootNode.get("contacts");
+            Flowable<Contact> clientFlowable = Flowable.create(emitter -> {
+                try {
+                    File file = new File("contacts.json");
+                    if (!file.exists()) {
+                        emitter.onComplete();
+                        return;
+                    }
 
-            if (contactsNode != null && contactsNode.isArray()) {
-                return objectMapper.convertValue(
-                        contactsNode,
-                        objectMapper.getTypeFactory().constructCollectionType(List.class, Contact.class)
-                );
-            }
+                    JsonNode rootNode = objectMapper.readTree(file);
+                    JsonNode contactsNode = rootNode.get("contacts");
 
+                    if (contactsNode != null && contactsNode.isArray()) {
+                        List<Contact> contacts = objectMapper.convertValue(
+                                contactsNode,
+                                objectMapper.getTypeFactory().constructCollectionType(List.class, Contact.class)
+                        );
+
+
+                        for (Contact contact : contacts) {
+                            emitter.onNext(contact);
+                        }
+                    }
+
+                    emitter.onComplete();
+                } catch (Exception e) {
+                    emitter.onError(e);
+                }
+            }, BackpressureStrategy.BUFFER);
+
+            return clientFlowable
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(Schedulers.single())
+                    .toList()
+                    .blockingGet();
+
+        } catch (Exception e) {
             return new ArrayList<>();
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read contacts from file", e);
         }
     }
+
 
     public void write(List<Contact> contacts) {
         Flowable.create((FlowableEmitter<Void> emitter) -> {
@@ -70,9 +94,6 @@ public class FileService {
                         throwable -> System.err.println("Error of creating contact: " + throwable.getMessage())
                 );
     }
-
-
-
 
 
 }
