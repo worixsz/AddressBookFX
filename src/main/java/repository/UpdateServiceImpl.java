542e6af1ed75b7fce8d9fe2272d20c89f6a082bd
azidinkun <azidinkun@icloud.com>
2024-12-23 01:35:20 +0600

    improved method with support for UPDATE in background thread

--- src/main/java/repository/UpdateServiceImpl.java
+++ src/main/java/repository/UpdateServiceImpl.java
@@ -1,6 +1,10 @@
 package repository;
 
 import fileService.FileService;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.FlowableEmitter;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import model.Contact;
 import service.UpdateService;
 
@@ -18,10 +22,27 @@ public class UpdateServiceImpl implements UpdateService {
     @Override
     public void update(Contact oldContact, Contact newContact) {
         List<Contact> contacts = fileService.read();
-        newContact.setId(oldContact.getId());
-        contacts.remove(oldContact);
-        contacts.add(newContact);
-        fileService.write(contacts);
+        Flowable.create((FlowableEmitter<Void> emitter) -> {
+                    try {
+                        newContact.setId(oldContact.getId());
+                        contacts.remove(oldContact);
+                        contacts.add(newContact);
+                        fileService.write(contacts);
+                        emitter.onComplete();
+                    } catch (Exception e) {
+                        emitter.onError(e);
+
+                    }
+
+                }, BackpressureStrategy.BUFFER)
+                .subscribeOn(Schedulers.io()).
+                observeOn(Schedulers.single())
+                .subscribe(
+                        (_) -> System.out.println("Contact success updated!"),
+                        throwable -> System.err.println("Error of updating contact: " + throwable.getMessage())
+                );
+
+
     }
 
 
